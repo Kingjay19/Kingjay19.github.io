3f810f8e009f71a9a0ced6eb07b5b895e6340229
Kingjay19 <78641046+Kingjay19@users.noreply.github.com>
2022-08-07 15:22:57 +0100

    Robofriends

---
+++ src/robots.js
@@ -0,0 +1,80 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Roman Reigns',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Brock Lesnar',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Seth Rollins',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Cody Rhose',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Jey Uso',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Jimmy Uso',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Randy Orton',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Riddle',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Rey Mysterio',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Edge',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Drew McIntyre',
+    username: 'Moriah.Stanton',
+    email: 'Warriorg@scottish.biz'
+  },
+  {
+    id: 12,
+    name: 'Sheamus',
+    username: 'Moriah.Stanton',
+    email: 'Bruth@irish.biz'
+  },
+  {
+    id: 13,
+    name: 'The Undertaker',
+    username: 'Moriah.Stanton',
+    email: 'Undertaker@wwe.biz'
+  }
+];
